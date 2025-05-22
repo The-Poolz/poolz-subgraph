837fcc6b58178655fbed6a2bb39b48fc0b5381fa
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-05-22 10:59:21 +0300

    simplify updateAllowedContract

--- src/extendedEntities/allowedContracts.ts
+++ src/extendedEntities/allowedContracts.ts
@@ -3,12 +3,8 @@ import { AllowedContract } from "../../generated/schema"
 
 export function updateAllowedContract(contractAddress: Bytes, status: boolean, blockTimestamp: BigInt): void {
     if (status) {
-        let allowedContract = AllowedContract.load(contractAddress)
-        if (allowedContract == null) {
-            allowedContract = new AllowedContract(contractAddress)
-            allowedContract.contractAddress = contractAddress
-        }
-
+        let allowedContract = new AllowedContract(contractAddress)
+        allowedContract.contractAddress = contractAddress
         allowedContract.status = status
         allowedContract.blockTimestamp = blockTimestamp
         allowedContract.save()
